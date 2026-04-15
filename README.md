0ad2822da41a2a390d7566108c242a02aa464090
IMSUN9 <sungu4790@naver.com>
2026-04-15 12:31:35 +0900

    feat: solve problem01

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sparta Algorithm Code Kata
+
+## [Problem01](./src/Problem01.java)
+
+### 두 수의 차 구하기
+- [x] 매개변수로 들어온 num1, num2의 차 구하기 
---
+++ src/Problem01.java
@@ -0,0 +1,16 @@
+public class Problem01 {
+
+    public static void main(String[] args) {
+
+        Solution01 solution01 = new Solution01();
+        System.out.println(solution01.solution(1,2));
+    }
+
+}
+
+class Solution01 {
+
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
