55cee8cf1909a40421cb8586b964d7587bdc3572
IMSUN9 <sungu4790@naver.com>
2026-04-16 09:40:39 +0900

    feat: 두 수의 곱 문제 풀이

---
+++ src/Problem02.java
@@ -0,0 +1,15 @@
+public class Problem02 {
+
+    public static void main(String[] args) {
+
+        Solution02 solution02 = new Solution02();
+        System.out.println(solution02.solution(3, 4));
+    }
+}
+
+class Solution02 {
+
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
