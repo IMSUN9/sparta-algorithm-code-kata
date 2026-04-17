fe01c43e9fb35463dac1dc6a79b4e30999f1d2f4
IMSUN9 <sungu4790@naver.com>
2026-04-17 09:17:07 +0900

    feat: 두 수의 나눈 몫 문제 풀이

---
+++ src/Problem03.java
@@ -0,0 +1,13 @@
+public class Problem03 {
+    public static void main(String[] args) {
+
+        Solution03 solution03 = new Solution03();
+        System.out.println(solution03.solution(10,5));
+    }
+}
+
+class Solution03 {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
