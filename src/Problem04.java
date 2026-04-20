f3cb0b6ecfcb650f5428c33e2852275b1a6cf9e7
IMSUN9 <sungu4790@naver.com>
2026-04-20 09:17:44 +0900

    feat: 두 수의 나눈 합 문제 풀이

---
+++ src/Problem04.java
@@ -0,0 +1,14 @@
+public class Problem04 {
+
+    public static void main(String[] args) {
+        Solution04 solution04 = new Solution04();
+        System.out.println(solution04.solution(1,2));
+    }
+}
+
+class Solution04 {
+
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
