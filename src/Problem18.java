392ba4b544267d483386f44a34ebe2f19b16b023
IMSUN9 <sungu4790@naver.com>
2026-05-11 09:18:02 +0900

    문자열을 정수로 바꾸기 문제 풀이

---
+++ src/Problem18.java
@@ -0,0 +1,20 @@
+public class Problem18 {
+
+    public static void main(String[] args) {
+
+        Solution18 solution18 = new Solution18();
+
+        System.out.println(solution18.solution("1234"));
+
+    }
+}
+
+class Solution18
+{
+    public int solution(String s) {
+
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
